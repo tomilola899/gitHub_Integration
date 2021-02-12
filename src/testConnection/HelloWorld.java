331caa2429067926475d168132851bbbb2e25749
Tommy <Tommy@LAPTOP-V6VDID38>
2021-02-11 22:22:00 -0500

    Created HelloWorld

--- src/testConnection/HelloWorld.java
+++ src/testConnection/HelloWorld.java
@@ -3,10 +3,8 @@ package testConnection;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
 		System.out.println("Hello Git");
+
 	}
 
 }
